5cfcbb832e9b868c271ff985a11772a041f4519d
Fajerwolf <fajerwolf95@gmail.com>
2023-02-01 13:16:53 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Zadanie domowe dzień 4
+var name = "Ewa";
+bool female = true;
+var age = 33;
+if (age < 30 && female)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age < 18 && !female)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Kto inny");
+}
